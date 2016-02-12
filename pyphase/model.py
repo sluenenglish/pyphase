509c63b6128073819f9aa92aa293154452ed4cfd
Sam Luen-English <sluenenglish@gmail.com>
2016-02-12 13:23:42 +0000

    Renamed variables to meaningful names

--- pyphase/model.py
+++ pyphase/model.py
@@ -3,16 +3,16 @@ import numpy as np
 
 class phase_model(object):
     
-    def __init__(self, T, pi):
-        self.phases = pi.shape[1]
-        self.k = 2 * self.phases - 1
-        self.T = T
-        self.pi = pi
-        self.e = np.ones((T.shape[0] , 1))
-        self.t = -T.dot(self.e)
+    def __init__(self, phase_type_generator, initial_state):
+        self.phases = initial_state.shape[1]
+        self.number_of_parameters = 2 * self.phases - 1
+        self.phase_type_generator = phase_type_generator
+        self.initial_state = initial_state
+        self.e = np.ones((self.phases , 1))
+        self.exit_rate_vector = - phase_type_generator.dot(self.e)
         
     def _pdf(self, y):  
-        return self.pi.dot(expm(self.T*y)).dot(self.t).reshape(-1)
+        return self.initial_state.dot(expm(self.phase_type_generator * y)).dot(self.exit_rate_vector).reshape(-1)
     
     def pdf(self, data):
         return np.array([self._pdf(i) for i in data])
@@ -23,4 +23,4 @@ class phase_model(object):
         return log_prob.sum()
 
     def aic(self, data):
-        return 2 * self.k - 2 * self.log_likelyhood(data)
+        return 2 * self.number_of_parameters - 2 * self.log_likelyhood(data)
